de71e7357f354a17976ed9419affb459b1950b01
宋凯 <kai.songsh@ele.me>
2016-11-07 21:41:48 +0800

    完成主界面抽离

--- MyApplication/app/src/main/java/happy/me/violet/violetbinding/MainActivity.java
+++ MyApplication/app/src/main/java/happy/me/violet/violetbinding/MainActivity.java
@@ -1,21 +1,25 @@
 package happy.me.violet.violetbinding;
 
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import happy.me.violet.violetbinding.databinding.MainPageBinding;
 import happy.me.violet.violetbinding.util.DataUtils;
 
-public class MainActivity extends AppCompatActivity {
-
-    private MainPageBinding mainPageBinding;
+public class MainActivity extends BaseActivity<MainPageBinding> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mainPageBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        mainPageBinding.setMainPageData(DataUtils.getMainPageData(0));
-        mainPageBinding.executePendingBindings();
+    }
+
+    @Override
+    public void initDataBinding() {
+        databinding.setMainPageData(DataUtils.getMainPageData(0));
+        databinding.executePendingBindings();
+    }
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.activity_main;
     }
 }
